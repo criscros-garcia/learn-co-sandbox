fdc07b1ba13c925567f5fcb4e8cbaf6d6763f2a2
Cristian Rogelio Garcia Valdez <eminemko@hotmail.com>
2019-07-28 02:59:56 +0000

    Automatically backed up by Learn

---
+++ loppsVsIterators.rb
@@ -0,0 +1,10 @@
+
+
+primary_colors = ["Red", "Yellow", "Blue"]
+
+
+colores = Proc.new do |color|
+  puts "Primary Color #{color} is #{color.length} letters long."
+end
+
+primary_colors.each(colores) 
